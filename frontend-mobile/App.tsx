3521f09ad0419ba376581cc47b269f94e498556b
DouglasMonteles <silvadouglas328@gmail.com>
2021-05-25 18:52:51 -0300

    Add lib of styles and expo fonts

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Ubuntu_300Light,
+  Ubuntu_400Regular,
+  Ubuntu_500Medium,
+  Ubuntu_700Bold,
+} from '@expo-google-fonts/ubuntu';
+import AppLoading from 'expo-app-loading';
+import fonts from './src/styles/fonts';
 
 export default function App() {
+  const [ isFontsLoaded ] = useFonts({
+    Ubuntu_300Light,
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!isFontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>app works!</Text>
     </View>
   );
 }
---
+++ frontend-mobile/src/styles/fonts.ts
@@ -0,0 +1,6 @@
+export default {
+  heading_strong: 'Ubuntu_700Bold',
+  heading_light: 'Ubuntu_300Light',
+  text_regular: 'Ubuntu_400Regular',
+  text_medium: 'Ubuntu_500Medium',
+};
